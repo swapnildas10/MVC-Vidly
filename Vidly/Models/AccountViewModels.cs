bd944c724401102c24cd89f5bb98cd460a2a6594
swapnildas10 <swapnildas@github.com>
2017-09-15 00:13:36 -0700

    Add Driving License in account authorization

--- Vidly/Models/AccountViewModels.cs
+++ Vidly/Models/AccountViewModels.cs
@@ -63,7 +63,11 @@ namespace Vidly.Models
     }
 
     public class RegisterViewModel
-    {
+    {   
+        [Required]
+        [Display(Name = "Driving License")]
+        public string DrivingLicense { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
