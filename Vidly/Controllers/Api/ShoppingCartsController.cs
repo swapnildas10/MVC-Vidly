0917df06881eef8483abfaed4c42f7712f4929d8
swapnildas10 <swapnildas@github.com>
2017-10-13 03:28:12 -0700

    Added SavedMoviesApiController

--- Vidly/Controllers/Api/ShoppingCartsController.cs
+++ Vidly/Controllers/Api/ShoppingCartsController.cs
@@ -6,28 +6,34 @@ using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
 using Glimpse.Core.Extensions;
 using Microsoft.AspNet.Identity;
 using Vidly.Models;
+using Vidly.ViewModels;
 using WebGrease.Css.Extensions;
 
 namespace Vidly.Controllers.Api
 {
     public class ShoppingCartsController : ApiController
     {
+
         private ApplicationDbContext _context;
         public ShoppingCartsController()
         {
+
            _context=new ApplicationDbContext();
         }
-        [HttpPost]
+        [System.Web.Http.HttpPost]
         public IHttpActionResult AddMovieToShoppingCart(int id )
         {
             var uid = HttpContext.Current.User.Identity.GetUserId();
-          
-            var cartitem = _context.ShoppingCarts.Where(s => s.User == uid);
+            var movies = _context.Movies.Single(m => m.Id == id);
+            var cartitem = _context.ShoppingCarts.Include(s=>s.Movie).Where(s => s.User == uid);
             if (cartitem.Any(c => c.Movie.Id == id))
-                return BadRequest("Already in Cart");
+                return Content(HttpStatusCode.Forbidden, "Already present in your cart");
+            if (movies.NumberAvailable ==0)
+                return Content(HttpStatusCode.Forbidden,"Out of Stock");
             var movie = _context.Movies.Single(m => m.Id == id);
             ShoppingCart cart = new ShoppingCart
             {
@@ -40,8 +46,8 @@ namespace Vidly.Controllers.Api
             _context.SaveChanges();
             return Ok();
         }
-        [HttpGet]
-        
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/shoppingcarts")]
         public IHttpActionResult GetItemsforShoppingCart()
         {
             var uid = HttpContext.Current.User.Identity.GetUserId();
@@ -57,10 +63,48 @@ namespace Vidly.Controllers.Api
            
             return Ok(total);
         }
-        
-        [HttpDelete]
+        [System.Web.Http.HttpGet]
+         [System.Web.Http.Route("api/savedmovies")]
+        public IHttpActionResult GetSavedItems()
+        {
+            var uid = HttpContext.Current.User.Identity.GetUserId();
+            var saveditems = _context.SavedMovies.Include(m => m.Movie).Where(s => s.User == uid).ToList();
+            if(saveditems.Count<=0)
+            return NotFound();
+            List<SavedMoviesViewModel> savedMovies = new List<SavedMoviesViewModel>();
+             foreach (SavedMovie savedMovie in saveditems)
+            {
+               savedMovies.Add(new SavedMoviesViewModel
+               {
+                   MovieId = savedMovie.Movie.Id,
+                   Name = savedMovie.Movie.Name,
+                   Stock = savedMovie.Movie.NumberAvailable
+               });
+            }
+             
+            return Ok(savedMovies);
+        }
+        [System.Web.Http.HttpPut]
+        public IHttpActionResult SaveMovie(int id)
+        {
+            var movie = _context.Movies.Single(m => m.Id == id);
+            var uid = HttpContext.Current.User.Identity.GetUserId();
+            var savedMovies = _context.SavedMovies.Include(m => m.Movie).Where(m => m.User == uid);
+            if (savedMovies.Where(m => m.Movie.Id == id).ToList().Any())
+            return Content(HttpStatusCode.Forbidden, "Duplicate Entry");
+
+            _context.SavedMovies.Add(new SavedMovie
+            {
+                Movie = movie,
+                User = uid
+            });
+            _context.SaveChanges();
+            return Ok("Saved for Later");
+        }
+        [System.Web.Http.HttpDelete]
         public IHttpActionResult DeleteItemfromShoppingCart(int id)
         {
+
             var uid = HttpContext.Current.User.Identity.GetUserId();
             var item = _context.ShoppingCarts.Single(s => s.User == uid && s.Movie.Id == id);
            
---
+++ Vidly/ViewModels/SavedMoviesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.ViewModels
+{
+    public class SavedMoviesViewModel
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public int MovieId { get; set; }
+    }
+}
