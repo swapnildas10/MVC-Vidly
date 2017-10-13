53e7876916f7a8714c5534bca87b832fa23ab281
swapnildas10 <swapnildas@github.com>
2017-10-12 20:28:33 -0700

    added SavedList DBLayer

---
+++ Vidly/Models/SavedMovie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class SavedMovie
+    {
+        public int Id { get; set; }
+        [Required]
+        public string User { get; set; }
+
+        [Required]
+        public Movie Movie { get; set; }
+    }
+}
